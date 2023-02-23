ace1a9c06cd85573e0dc9d0f0a29fbb6e0b3c291
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2023-02-23 22:09:19 +0530

    added basic UI

--- script.js
+++ script.js
@@ -44,40 +44,37 @@ function getComputerChoice() {
 	return choices[Math.floor(Math.random() * 3)]
 }
 
-function game() {
-	let player = 0,
-		computer = 0
+function game(playerSelection) {
+	// Generating a random choice for the computer
+	let computerSelection = getComputerChoice()
 
-	for (let i = 0; i < 5; i++) {
-		let playerSelection = prompt('Rock, Paper or Scissors?')
-		// Taking user input through prompt
-		let computerSelection = getComputerChoice()
-		// Generating a random choice for the computer
+	roundResults = playRound(playerSelection, computerSelection)
+	document.getElementById(
+		'playerSelection'
+	).innerText = `Player chose: ${playerSelection}`
+	document.getElementById(
+		'computerSelection'
+	).innerText = `Computer chose: ${computerSelection}`
+	document.getElementById('feedback').innerText = roundResults.message
 
-		console.log('Computer choice:', computerSelection)
-		console.log('Player choice:', playerSelection)
-
-		roundResults = playRound(playerSelection, computerSelection)
-		console.log(roundResults.message)
-
-		if (roundResults.result === 'W') {
-			player++
-		} else if (roundResults.result === 'L') {
-			computer++
-		}
-		// To check who won the current round
-
-		roundResults = null
+	// To check who won the current round
+	if (roundResults.result === 'W') {
+		player++
+	} else if (roundResults.result === 'L') {
+		computer++
 	}
 
-	if (player > computer) {
-		console.log('Player Wins the game!')
-	} else if (computer > player) {
-		console.log('Computer Wins the game!')
-	} else {
-		console.log('The Match is a tie')
+	document.getElementById('playerScore').innerText = `Player Score: ${player}`
+	document.getElementById(
+		'computerScore'
+	).innerText = `Computer Score: ${computer}`
+
+	if (player === 5) {
+		document.getElementById('endResult').innerText = 'Player Wins'
+	} else if (computer === 5) {
+		document.getElementById('endResult').innerText = 'Computer Wins'
 	}
-	// Logic to check who won the entire match
 }
 
-game()
+let player = 0,
+	computer = 0
